21590921d120a46f45e0cb256223400fc8c1c4ad
unknown <salhiakram05@gmail.com>
2025-06-01 15:07:31 +0100

    test

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,14 +1,14 @@
 <?php
+
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
     
     protected $proxies = '*';
 
-    
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+   
+    protected $headers = Middleware::HEADER_X_FORWARDED_ALL;
 }
